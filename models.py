2ed4d260c6ea596e0cef523bd294403fdfa08418
Taechawat Konkaew <luangta_5@hotmail.com>
2021-01-17 17:45:16 +0700

    Change model architecture

--- models.py
+++ models.py
@@ -13,43 +13,19 @@ from tensorflow.keras.layers import (
 from tensorflow.keras.models import Model
 
 
-def create_cnn_model(image_size: Tuple[int, int], num_classes: int) -> Model:
-    input_ = Input(shape=(*image_size, 3), name="input")
-
-    x = Conv2D(64, (3, 3), activation="relu", name="conv_1_1")(input_)
-    x = Conv2D(64, (3, 3), activation="relu", name="conv_1_2")(x)
-    x = MaxPooling2D((3, 3))(x)
-
-    x = Conv2D(128, (3, 3), activation="relu", name="conv_2_1")(x)
-    x = Conv2D(128, (3, 3), activation="relu", name="conv_2_2")(x)
-    x = MaxPooling2D((2, 2))(x)
-
-    x = Conv2D(256, (3, 3), activation="relu", name="conv_3_1")(x)
-    x = Conv2D(256, (3, 3), activation="relu", name="conv_3_2")(x)
-    x = MaxPooling2D((3, 3))(x)
-
-    x = Flatten()(x)
-    x = Dense(512, activation="relu")(x)
-    x = Dense(128, activation="relu")(x)
-    x = Dropout(0.4)(x)
-
-    prediction = Dense(num_classes, activation="softmax")(x)
-
-    return Model(inputs=input_, outputs=prediction)
-
-
-def create_inception_model(image_size: Tuple[int, int], num_classes: int) -> Model:
+def create_model(image_size: Tuple[int, int], num_classes: int) -> Model:
     inception_v3 = InceptionV3(
         input_shape=(*image_size, 3), include_top=False, weights="imagenet"
     )
     for layer in inception_v3.layers:
         layer.trainable = False
 
-    output = inception_v3.output
+    # output = inception_v3.output
+    output = inception_v3.get_layer("mixed9").output
+
     x = Flatten()(output)
-    x = Dense(512, activation="relu")(x)
-    x = Dense(128, activation="relu")(x)
-    x = Dropout(0.4)(x)
+    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.2)(x)
 
     prediction = Dense(num_classes, activation="softmax")(x)
 
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from tensorflow.keras.callbacks import (
 )
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from models import create_inception_model
+from models import create_model
 
 dataset_dir = "datasets"
 
@@ -21,35 +21,25 @@ IMAGE_SIZE = (150, 150)
 
 classes = os.listdir(train_dir)
 num_classes = len(classes)
-num_epochs = 50
+num_epochs = 15
 
-data_generator = ImageDataGenerator(
+data_augment_generator = ImageDataGenerator(
     rescale=1.0 / 255,
     width_shift_range=0.2,
     height_shift_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True,
-    validation_split=0.3,
+    vertical_flip=True,
     fill_mode="nearest",
 )
 
 
-train_data_generator = data_generator.flow_from_directory(
-    directory=train_dir,
-    target_size=IMAGE_SIZE,
-    class_mode="categorical",
-    subset="training",
-    shuffle=True,
-    batch_size=64,
+train_data_generator = data_augment_generator.flow_from_directory(
+    train_dir, batch_size=32, class_mode="categorical", target_size=(150, 150)
 )
+data_generator = ImageDataGenerator(rescale=1.0 / 255)
 
 test_data_generator = data_generator.flow_from_directory(
-    directory=test_dir,
-    target_size=IMAGE_SIZE,
-    class_mode="categorical",
-    subset="validation",
-    shuffle=True,
-    batch_size=64,
+    test_dir, batch_size=128, class_mode="categorical", target_size=(150, 150)
 )
 
 model = create_inception_model(image_size=IMAGE_SIZE, num_classes=num_classes)
@@ -62,24 +52,17 @@ checkpoint_callback = ModelCheckpoint(
     best_model_path, monitor="val_accuracy", save_best_only=True, verbose=1
 )
 
-early_stopping = EarlyStopping(
-    monitor="val_loss", patience=2, min_delta=0.001, mode="auto"
+reduce_lr = ReduceLROnPlateau(
+    monitor="val_accuracy", patience=2, verbose=1, factor=0.25, min_lr=0.000003
 )
 
-reduce_lr = ReduceLROnPlateau(
-    monitor="val_loss", factor=0.2, patience=2, min_lr=0.000003
-)
+
 csv_logger = CSVLogger("training_log.csv")
 
 history = model.fit(
     train_data_generator,
-    steps_per_epoch=train_data_generator.n // train_data_generator.batch_size,
     validation_data=test_data_generator,
-    validation_steps=test_data_generator.n // test_data_generator.batch_size,
     epochs=num_epochs,
-    callbacks=[checkpoint_callback, early_stopping, reduce_lr, csv_logger],
+    callbacks=[checkpoint_callback, reduce_lr, csv_logger],
     verbose=1,
 )
-
-model.save("inception_model.h5")
-model.save_weights("inception_model.h5")
